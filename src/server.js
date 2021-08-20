280498e4bb8112ab26974634a664aa50b5706469
zcrossoverz <56151228+zcrossoverz@users.noreply.github.com>
2021-08-20 08:24:52 +0700

    fix

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
 import bodyParser from "body-parser";
-// require("dotenv").config();
+require("dotenv").config();
 let app = express();
 
 viewEngine(app);
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 initWebRoutes(app);
 
-let port = 5000;
+let port = process.env.PORT || 5000;
 app.listen(port, ()=>{
     console.log('Chatbot runnin...');
 });
